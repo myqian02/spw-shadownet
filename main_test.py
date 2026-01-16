aaeaa4bfc15aa917dbda9fbc77a9340375d6e594
myqian02 <myqian02@gmail.com>
2026-01-16 13:59:10 +0800

    main_test.py

--- main_test.py
+++ main_test.py
@@ -173,135 +173,37 @@ if __name__ == '__main__':
 
         with torch.no_grad():
             for img_name in tqdm(image_names):
-                
-                # gt_image = imageio.imread(os.path.join(gt_path, img_name), pilmode='RGB')
-                # shadow_image = imageio.imread(os.path.join(shadow_path, img_name))
-                # # result_path = os.path.join(args.save_dir, 'results', args.dataset_name, args.model_path[-14:])
-                # # cv2.imwrite(os.path.join(result_path, img_name[:-4] + '_s.png'), cv2.cvtColor(shadow_image, cv2.COLOR_BGR2RGB))
-                # shadow_image = util.uint2single(shadow_image)
-                # # shadow_illumination = imageio.imread(os.path.join(shadow_illumination_path, img_name))
-                # # shadow_illumination = shadow_illumination[:, :, 0:1]
-
-                # shadow_input = util.single2tensor4(shadow_image)
-                # shadow_input, pad_left, pad_right, pad_top, pad_bottom = pad_tensor(shadow_input)
-                # if cuda:
-                #     shadow_input = shadow_input.cuda()
-                # pred_img, pred_illumination, pred_reflectance = model(shadow_input)
-                # pred_illumination = pad_tensor_back(pred_illumination, pad_left, pad_right, pad_top, pad_bottom)
-                # pred_img = pad_tensor_back(pred_img, pad_left, pad_right, pad_top, pad_bottom)
-                # pred_reflectance = pad_tensor_back(pred_reflectance, pad_left, pad_right, pad_top, pad_bottom)
-                # pred_reconstruct = pred_illumination * pred_reflectance
-
-                # gt_tensor = util.single2tensor4(gt_image).cuda()
-                # intensity_lpip = lpips_model(pred_img, gt_tensor)
-
-
-                # pred_illumination = util.tensor2single(pred_illumination)
-                # pred_reflectance = util.tensor2single(pred_reflectance)
-                # pred_reconstruct = util.tensor2single(pred_reconstruct)
-                # pred_img = util.tensor2single(pred_img)
-
-                # torch.cuda.synchronize()
-                # pred_illumination = util.single2uint(pred_illumination)
-                # pred_reflectance = util.single2uint(pred_reflectance)
-                # pred_reconstruct = util.single2uint(pred_reconstruct)
-                # # pred_illumination = cv2.cvtColor(pred_illumination, cv2.COLOR_GRAY2RGB)
-
-                # pred_img = util.single2uint(pred_img)
-                # intensity_psnr = util.calculate_psnr(pred_img, gt_image)
-                # intensity_ssim = util.calculate_ssim(pred_img, gt_image)
-                
-                # intensity_psnrs.append(intensity_psnr)
-                # intensity_ssims.append(intensity_ssim)
-                # intensity_lpips.append(intensity_lpip)
-
-                # import pdb; pdb.set_trace()
-
                 gt_image = imageio.imread(os.path.join(gt_path, img_name), pilmode='RGB')
                 shadow_image = imageio.imread(os.path.join(shadow_path, img_name))
-                # shadow_image = cv2.resize(shadow_image, (256, 256), fx=0, fy=0)
-                # gt_image = cv2.resize(gt_image, (256, 256), fx=0, fy=0)
-                # result_path = os.path.join(args.save_dir, 'results', args.dataset_name, args.model_path[-14:])
-                # cv2.imwrite(os.path.join(result_path, img_name[:-4] + '_s.png'), cv2.cvtColor(shadow_image, cv2.COLOR_BGR2RGB))
                 shadow_image = util.uint2single(shadow_image)
-                # shadow_illumination = imageio.imread(os.path.join(shadow_illumination_path, img_name))
-                # shadow_illumination = shadow_illumination[:, :, 0:1]
 
                 shadow_input = util.single2tensor4(shadow_image)
                 shadow_input, pad_left, pad_right, pad_top, pad_bottom = pad_tensor(shadow_input)
                 if cuda:
                     shadow_input = shadow_input.cuda()
-                start_time = time.time()
-                # pred_img, _, _ = model(shadow_input, isshadow=True)
-                # print(shadow_input.shape)
                 pred_img, _ = model(shadow_input)
-                end_time = time.time()
-                # print(end_time - start_time)
-                # pred_illumination = pad_tensor_back(pred_illumination, pad_left, pad_right, pad_top, pad_bottom)
                 pred_img = pad_tensor_back(pred_img, pad_left, pad_right, pad_top, pad_bottom)
-                # print(pred_img.shape, pad_right, pad_left, pad_top, pad_bottom, end_time-start_time, 1/(end_time-start_time))
-                # pred_reflectance = pad_tensor_back(pred_reflectance, pad_left, pad_right, pad_top, pad_bottom)
-                # pred_reconstruct = pred_illumination * pred_reflectance
 
                 gt_tensor = util.single2tensor4(gt_image/255).cuda()
                 intensity_lpip = lpips_model(pred_img, gt_tensor)
-
-                # pred_illumination = util.tensor2single(pred_illumination)
-                # pred_reflectance = util.tensor2single(pred_reflectance)
-                # pred_reconstruct = util.tensor2single(pred_reconstruct)
                 pred_img = util.tensor2single(pred_img)
 
                 torch.cuda.synchronize()
-                # pred_illumination = util.single2uint(pred_illumination)
-                # pred_reflectance = util.single2uint(pred_reflectance)
-                # pred_reconstruct = util.single2uint(pred_reconstruct)
-                # pred_illumination = cv2.cvtColor(pred_illumination, cv2.COLOR_GRAY2RGB)
 
                 pred_img = util.single2uint(pred_img)
-                # print(pred_img.shape)
-                intensity_psnr = util.calculate_psnr(pred_img, gt_image, 2)
-                intensity_ssim = util.calculate_ssim(pred_img, gt_image, 2)
-                intensity_rmse = util.calculate_rmse(pred_img, gt_image, 0)
+                intensity_psnr = util.calculate_psnr(pred_img, gt_image)
+                intensity_ssim = util.calculate_ssim(pred_img, gt_image)
+                intensity_rmse = util.calculate_rmse(pred_img, gt_image)
                 intensity_niqe = util.calculate_niqe(gt_image)
-                # print(f"ID: {img_name}; NIQE: {intensity_niqe}; Time: {end_time-start_time}")
-                # intensity_rmse = np.abs(cv2.cvtColor(pred_img, cv2.COLOR_RGB2LAB) - cv2.cvtColor(gt_image, cv2.COLOR_RGB2LAB)).mean() * 3
-                
-                # intensity_psnrs.append(intensity_psnr)
-                # intensity_ssims.append(intensity_ssim)
-                # intensity_rmses.append(intensity_rmse)
-                # intensity_lpips.append(intensity_lpip)
-                # total_time.append(end_time-start_time)
-                if intensity_psnr > 1.70:
-                # if intensity_psnr > 16.5:
-                    intensity_psnrs.append(intensity_psnr)
-                    intensity_ssims.append(intensity_ssim)
-                    intensity_rmses.append(intensity_rmse)
-                    intensity_lpips.append(intensity_lpip)
-                    intensity_niqes.append(intensity_niqe)
-                    total_time.append(end_time-start_time)
-                else:
-                    print(img_name, intensity_psnr)
-
-                # if intensity_psnr < 29:
-                #     print(img_name, intensity_psnr, intensity_ssim, intensity_rmse, intensity_lpip)
 
                 if args.save_results:
                     result_path = os.path.join(args.save_dir, 'results', args.dataset_name, args.model_path[-14:])
-                    # print(pred_img.shape, pred_illumination.shape)
                     makedir(result_path)
                     imageio.imsave(os.path.join(result_path, img_name[:-4] + '.png'), pred_img)
-                    # imageio.imsave(os.path.join(result_path, img_name[:-4] + '_illumination.png'), pred_illumination)
-                    # imageio.imsave(os.path.join(result_path, img_name[:-4] + '_reflectance.png'), pred_reflectance)
-                    # imageio.imsave(os.path.join(result_path, img_name[:-4] + '_reconstruct.png'), pred_reconstruct)
-                    # imageio.imsave(os.path.join(result_path, img_name[:-4] + '_illumination.png'),
-                    #                pred_illumination)
-                    # cv2.imwrite(os.path.join(result_path, img_name[:-4] +"_illumination.png"),
-                    #             (pred_illumination*255).astype(np.uint8))
 
         print('Intensity PSNR: %04f' % (np.mean(intensity_psnrs)))
         print('Intensity SSIM: %04f' % (np.mean(intensity_ssims)))
         print('Intensity RMSE: %04f' % (np.mean(intensity_rmses)))
-        # print('Intensity LPIPS: %04f' % (np.mean(intensity_lpips)))
         print('Intensity LPIPS: %04f' % (sum(intensity_lpips)/len(image_names)))
         print('Intensity NIQES: %04f' % (sum(intensity_niqes)/len(image_names)))
         print('Everage Time Per Image: %04f' % (np.mean(total_time)))
@@ -309,12 +211,5 @@ if __name__ == '__main__':
         print('Elapsed Time: %04f' % (time.time() - begin_time))
         print('Image Counts: %04d' % (len(intensity_psnrs)))
 
-        # all_intensity_psnrs.append(np.mean(intensity_psnrs))
-        # all_model.append(model_name)
-        # print(np.mean(intensity_rmses), sum(intensity_rmses)/len(image_names))
-
-    # metrics = pd.DataFrame({'Model': all_model, 'PSNR': all_intensity_psnrs})
-    # metrics_file_name = 'metrics_' + args.dataset_name + '.xlsx'
-    # metrics.to_excel(os.path.join(args.save_dir, 'results', metrics_file_name), index=False)
 
 
